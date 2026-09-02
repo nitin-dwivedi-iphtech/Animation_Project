346c541973a9be4c71f9705f0ab9c2b3f5f880f9
Nitin Dwivedi <nitin.dwivedi@iphtechnologies.com>
2026-09-02 10:36:31 +0530

    Initial Commit

---
+++ AnimationProject/AnimationProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationProjectApp.swift
+//  AnimationProject
+//
+//  Created by iPHTech 40 on 02/09/26.
+//
+
+import SwiftUI
+
+@main
+struct AnimationProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnimationProject
+//
+//  Created by iPHTech 40 on 02/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
